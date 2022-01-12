761811908ea2b8c21777deab137f56d604ebfbe7
Елисей <elisey@Elisejs-MacBook-Pro.local>
2022-01-12 17:48:55 +0300

    Shadow bug fix

--- src/Image.js
+++ src/Image.js
@@ -7,10 +7,17 @@ class Image extends Component {
 	}
 	finishLoad = () => {
 		this.props.finishLoading()
+		if (this.props.mockup == 0) {
+			document.querySelector('.js-result-image').classList.add('has-shadow')
+		}
+		if (this.props.mockup == 1) {
+			document.querySelector('.js-result-image').classList.remove('has-shadow')
+		}
+		
 	}
 
 	componentDidUpdate(prevProps) {
-  // Typical usage (don't forget to compare props):
+
   if (this.props.category !== prevProps.category) {
     this.startLoad()
   }
@@ -41,7 +48,7 @@ class Image extends Component {
 		this.props.mockup + 
 		this.props.color
 		return <img 
-		className = {`main__result-image result-image ` + `${this.props.mockup ? "no-shadow" : ""}`}
+		className = {`main__result-image result-image js-result-image`}
 		src={`./ui/${this.src}.png`} 
 		alt="" 
 		onLoad={() => this.finishLoad()}
