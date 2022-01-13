199f75571dcef8f8c1045351b5f13309f277e8fb
Елисей <elisey@Elisejs-MacBook-Pro.local>
2022-01-13 19:15:36 +0300

    404

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import NotFound from './NotFound'
-import {
-	BrowserRouter,
-	Routes,
-	Route
-} from 'react-router-dom'
+// import NotFound from './NotFound'
+// import {
+// 	BrowserRouter,
+// 	Routes,
+// 	Route
+// } from 'react-router-dom'
 
-ReactDOM.render(
-	<BrowserRouter>
-		<Routes>
-			<Route path="/" element={<App />}></Route>
-			<Route path="*" element={<NotFound />}></Route>
-		</Routes>
-	</BrowserRouter>
+// ReactDOM.render(
+// 	<BrowserRouter>
+// 		<Routes>
+// 			<Route path="/" element={<App />}></Route>
+// 			<Route path="*" element={<NotFound />}></Route>
+// 		</Routes>
+// 	</BrowserRouter>
 	
 	
-	, document.getElementById('root')
-)
+// 	, document.getElementById('root')
+// )
+
+ReactDOM.render(<App />, document.getElementById('root'))
