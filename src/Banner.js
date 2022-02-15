258ac532536871ff61ab0d7b1180e7c438eadf81
Елисей <elisey@Elisejs-MacBook-Pro.local>
2022-02-15 11:28:08 +0300

    Banner

--- src/Banner.js
+++ src/Banner.js
@@ -3,7 +3,7 @@ import React from "react";
 class Banner extends React.Component {
 	render() {
 		return (
-			<a className="banner" href="https://producthunt.com/" target="_blank">
+			<a className="banner" href="https://producthunt.com/posts/ui-generator" target="_blank">
 				<span className="banner__text">Support us on Product Hunt! 😻</span>
 			</a>
 		);
