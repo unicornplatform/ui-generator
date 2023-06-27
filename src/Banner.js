5f637f4e5f1ef489689c77b9723177927d93d4eb
Elisey <elisgubarev@gmail.com>
2023-06-27 11:17:56 +0400

    Add PH banner

--- src/Banner.js
+++ src/Banner.js
@@ -1,13 +1,34 @@
 import React from "react";
 
 class Banner extends React.Component {
-	render() {
-		return (
-			<a className="banner" href="https://producthunt.com/posts/ui-generator" target="_blank">
-				<span className="banner__text">Support us on Product Hunt! 😻</span>
-			</a>
-		);
-	}
+  render() {
+    return (
+      <div className="banner">
+        {" "}
+        <div className="banner__content">
+          {" "}
+          <div className="banner__text">
+            {" "}
+            <b>
+              <img
+                className="banner__img"
+                src="https://user-uploads-unicorn.s3.amazonaws.com/product_hunt_kitty.png"
+              />{" "}
+              AI is here! 🪄
+            </b>{" "}
+            We're live on Product Hunt launching our GPT4 integration.{" "}
+            <a
+              target="_blank"
+              className="banner__link"
+              href="https://www.producthunt.com/posts/unicorn-platform-ai"
+            >
+              SUPPORT US
+            </a>{" "}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Banner;
